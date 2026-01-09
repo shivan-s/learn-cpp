1642d159e0a2f33dc88698634d61bfa9caa693f8
Shivan Sivakumaran <me@shivan.xyz>
2026-01-10 00:08:05 +1300

    wip - part 2

---
+++ 02-abstraction-mechamisms/2.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// 5 Built-in C++ types
+
+	// 1. Vector
+	vector<int> v = { 4, 5, 6 };
+	cout << v[2] << "\n";
+
+	// 2. String
+	string s = "This is a string";
+
+	// TODO
+
+	// 5 Custom C++ Types
+	// TODO
+};
